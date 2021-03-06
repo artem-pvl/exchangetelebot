5084e12686e8a307bef77858fe1ec3a12df661f5
Artem Pavlichenko <artem.pvl@gmail.com>
2021-03-06 16:04:04 +0300

    exchangetelebot-master: release

--- extensions.py
+++ extensions.py
@@ -1,21 +1,67 @@
-import config
 import redis
+import json
+import requests
+import datetime
+
+import config
 
 CURRENCIES_ENUM = {j: i for i, j in zip(config.CURRENCIES, config.CURRENCIES.values())}
 
 
-class ConvertException(Exception):
+class APIException(Exception):
+    pass
+
+
+class ConvertException(APIException):
+    pass
+
+
+class NoAccessException(APIException):
     pass
 
 
 class CurrencyConverter:
     @staticmethod
-    def get_currency_rate(currency_from: str, currency_to: str):
-        pass
+    def get_currency_api(currency_from: str):
+        req = requests.get(f"https://api.exchangeratesapi.io/latest?base={currency_from}")
+        if req.status_code == 200:
+            return req.content
+        else:
+            raise NoAccessException(f'Проблемы с сервером курсов валют: {req.status_code}')
+
+    @staticmethod
+    def get_price(base: str, quote: str, amount: float):
+
+        red = redis.Redis(host=config.REDIS_ACC['host'], port=config.REDIS_ACC['port'],
+                          password=config.REDIS_ACC['password'])
+        if not red:
+            raise NoAccessException(f'Проблемы с сервером кэширования')
+
+        red_today = json.loads(red.get('today'))
+        if not red_today or (red_today != datetime.date.today().isoformat()):
+            red_record = CurrencyConverter.get_currency_api(base)
+            red.set(base, red_record)
+            currency_rates = json.loads(red_record)
+            red.set('last_date', json.dumps(currency_rates['date']))
+            red_last_date = currency_rates['date']
+            red.set('today', json.dumps(datetime.date.today().isoformat()))
+        else:
+            red_last_date = json.loads(red.get('last_date'))
+            red_record = red.get(base)
+
+        if red_record:
+            currency_rates = json.loads(red_record)
+            if currency_rates['date'] == red_last_date:
+                return currency_rates['rates'][quote] * amount
+
+        api_res = CurrencyConverter.get_currency_api(base)
+        red.set(base, api_res)
+        currency_rates = json.loads(api_res)
+        return currency_rates['rates'][quote] * amount
 
     @staticmethod
     def currency_enum(conv_list: list):
-        if conv_list[0] in config.CURRENCIES:
+        if conv_list[0].upper() in config.CURRENCIES:
             currency = conv_list.pop(0)
         elif conv_list[0] in CURRENCIES_ENUM:
             currency = CURRENCIES_ENUM[conv_list.pop(0)]
@@ -29,20 +75,23 @@ class CurrencyConverter:
             conv_list.pop(0)
             conv_list.pop(0)
         else:
-            raise ConvertException(f'Не удалось обработать валюту {conv_list[0]}')
-        return currency
+            raise ConvertException(f'Не удалось обработать валюту: {conv_list[0]}')
+        return currency.upper()
 
     @staticmethod
-    def convert_currency(conv_string: str):
+    def convert_currency_str(conv_string: str):
         conv_list = conv_string.lower().split()
         currency_from = CurrencyConverter.currency_enum(conv_list)
         currency_to = CurrencyConverter.currency_enum(conv_list)
 
-        try:
-            amount = float(conv_list[0])
-        except ValueError:
-            raise ConvertException(f'Не удалось обработать количество {conv_list[0]}')
+        if conv_list:
+            try:
+                amount = float(conv_list[0])
+            except ValueError:
+                raise ConvertException(f'Не удалось обработать количество: {conv_list[0]}')
+        else:
+            amount = 1
 
-        print(currency_from, currency_to, amount)
+        rate = CurrencyConverter.get_price(currency_from, currency_to, amount)
 
-
+        return currency_from, currency_to, amount, rate
--- main.py
+++ main.py
@@ -1,6 +1,56 @@
+import telebot
+
+import config
+import extensions
+
+bot = telebot.TeleBot(config.BOT_TOKEN)
+
 
 def main():
-    pass
+    bot.polling(none_stop=True)
+
+
+@bot.message_handler(commands=["start"])
+def handle_start(message: telebot.types.Message):
+    txt_msg = f"Привет, {message.from_user.username}!\n" \
+              f"Я бот калькулятор валют. Считаю из одной валюты в другую по курсу European Central Bank.\n" \
+              f" Для помощи набери /help" \
+
+    bot.send_message(message.chat.id, txt_msg)
+
+
+@bot.message_handler(commands=["help"])
+def handle_start(message: telebot.types.Message):
+    txt_msg = "Для конвертации валют наберите <имя валюты цену которой вы хотите узнать> <имя валюты в которой надо " \
+              "узнать цену первой валюты> <количество первой валюты>\n" \
+              "Наименование валюты можно вводить названием (доллар сша рубль 100) или международным обозначением" \
+              " (USD RUB 100)\n" \
+              "/values - список валют\n" \
+              "/help - это сообщение"
+
+    bot.send_message(message.chat.id, txt_msg)
+
+
+@bot.message_handler(commands=["values"])
+def handle_start(message: telebot.types.Message):
+    txt_cur = "\n".join([f"{config.CURRENCIES[i]} ({i}) - {config.COUNTRIES[i]}" for i in config.CURRENCIES])
+    txt_msg = f'Список валют которые я умею считать:\n' \
+              f'{txt_cur}'
+
+    bot.send_message(message.chat.id, txt_msg)
+
+
+@bot.message_handler(content_types=['text'])
+def handle_start(message: telebot.types.Message):
+    try:
+        cur_from, cur_to, cur_amount, cur_rate = extensions.CurrencyConverter.convert_currency_str(message.text)
+    except extensions.ConvertException as ex:
+        bot.reply_to(message, f'{ex}')
+    except extensions.APIException as ex:
+        bot.reply_to(message, f'{ex}')
+    else:
+        msg_text = f"{round(cur_amount, 2)} {cur_from} = {round(cur_rate, 2)} {cur_to}"
+        bot.reply_to(message, msg_text)
 
 
 if __name__ == '__main__':
